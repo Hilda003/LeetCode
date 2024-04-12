d214bcb492bd618bc66dd63e9dca48562d6bf32f
Hilda003 <Hildatulwardah003@gmail.com>
2024-04-12 23:40:25 +0700

    update

---
+++ CodeWars/sum_array.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def sum_array(arr):
+        if not arr:
+            return 0
+        return sum(arr)
+    
+
+    print(sum_array([2, 4, 5, 6]))
---
+++ CodeWars/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    print(sum_of_numbers([1, 5.2, 4, 0, -1]))  # Output: 9.2
+    print(sum_of_numbers([]))                  # Output: 0
+    print(sum_of_numbers([-2.398]))             # Output: -2.398
